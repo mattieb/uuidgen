de799f8f46075f601c9e57501af04dc7100d30d1
Matt Behrens <matt@zigg.com>
2014-06-12 12:07:28 -0400

    Initial Commit

---
+++ uuidgen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  uuidgen
+//
+//  Created by Matt Behrens on 6/12/14.
+//  Copyright (c) 2014 Matt Behrens. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
